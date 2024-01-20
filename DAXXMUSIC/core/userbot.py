44a10db85a0a30b7e12be907d97245d0b22d7b1a
PB65-Aujla <140618546+PB65-Aujla@users.noreply.github.com>
2024-01-20 10:28:16 +0400

    Update userbot.py

--- DAXXMUSIC/core/userbot.py
+++ DAXXMUSIC/core/userbot.py
@@ -51,9 +51,7 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("HEROKUFREECC")
-              
-                await self.one.join_chat("ALLTYPECC")
+                await self.one.join_chat("musicbotv5")
             except:
                 pass
             assistants.append(1)
@@ -74,9 +72,7 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("HEROKUFREECC")
-                
-                await self.one.join_chat("ALLTYPECC")
+                await self.two.join_chat("musicbotv5")
             except:
                 pass
             assistants.append(2)
@@ -96,9 +92,7 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("HEROKUFREECC")
-                
-                await self.one.join_chat("ALLTYPECC")
+                await self.three.join_chat("musicbotv5")
             except:
                 pass
             assistants.append(3)
@@ -118,9 +112,7 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("HEROKUFREECC")
-                
-                await self.one.join_chat("ALLTYPECC")
+                await self.four.join_chat("musicbotv5")
             except:
                 pass
             assistants.append(4)
@@ -140,9 +132,7 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("HEROKUFREECC")
-                
-                await self.one.join_chat("ALLTYPECC")
+                await self.five.join_chat("musicbotv5")
             except:
                 pass
             assistants.append(5)
